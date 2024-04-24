c17388b9c8a80d8e62ee2c40efdd3b74d00d5f17
JiSangChoe <chlwltkd123@gmail.com>
2024-04-24 11:50:49 +0900

    2024.04.24

---
+++ back/src/main/java/com/estate/back/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.estate.back.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estate.back.entity.BoardEntity;
+
+// estate 데이터베이스의 board 테이블 작업을 위한 레포지토리
+public interface BoardRepository extends JpaRepository<BoardEntity, Integer>{
+    
+}
