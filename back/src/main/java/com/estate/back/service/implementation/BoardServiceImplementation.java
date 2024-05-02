dd522a7fffd84bd36731bf9305fadabc4c4ff73c
JiSangChoe <chlwltkd123@gmail.com>
2024-05-02 16:51:20 +0900

    2024.05.02.16:50

--- back/src/main/java/com/estate/back/service/implementation/BoardServiceImplementation.java
+++ back/src/main/java/com/estate/back/service/implementation/BoardServiceImplementation.java
@@ -3,8 +3,10 @@ package com.estate.back.service.implementation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import com.estate.back.dto.request.board.PostBoardRequestDto;
 import com.estate.back.dto.response.ResponseDto;
+import com.estate.back.dto.response.board.GetBoardListResponseDto;
 import com.estate.back.entity.BoardEntity;
 import com.estate.back.repository.BoardRepository;
 import com.estate.back.repository.UserRepository;
@@ -36,6 +38,22 @@ public class BoardServiceImplementation implements BoardService{
         }
 
         return ResponseDto.success();
+    }
+
+    @Override
+    public ResponseEntity<? super GetBoardListResponseDto> getBoardList() {
+        
+        try {
+
+            List<BoardEntity> boardEntities = boardRepository.findByOrderByReceptionNumberDesc();
+            return GetBoardListResponseDto.success(boardEntities);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+    
     }
     
 }
