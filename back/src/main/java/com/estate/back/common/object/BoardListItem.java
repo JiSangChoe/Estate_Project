445a7211bdee89fc3833fec45eb5fd6a3f204cc5
JiSangChoe <chlwltkd123@gmail.com>
2024-05-02 17:28:32 +0900

    2024.05.02.17:50

--- back/src/main/java/com/estate/back/common/object/BoardListItem.java
+++ back/src/main/java/com/estate/back/common/object/BoardListItem.java
@@ -21,12 +21,14 @@ public class BoardListItem {
     private Integer viewCount;
 
     private BoardListItem(BoardEntity boardEntity) throws Exception {
-        // 
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+        // 문자열을 데이터타입으로 바꾸고 "yy.MM.dd"형태로 데이터타입을 문자열로 바꾸는 코드
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date datetime = simpleDateFormat.parse(boardEntity.getWriteDatetime());
         simpleDateFormat = new SimpleDateFormat("yy.MM.dd");
         String writeDatetime = simpleDateFormat.format(datetime);
 
+        // 첫글자만 보이고 나머지는 *로 표시하기 위한 코드
         String writerId = boardEntity.getWriterId();
         writerId = writerId.substring(0, 1) + 
         "*".repeat(writerId.length() - 1);
@@ -42,6 +44,7 @@ public class BoardListItem {
     public static List<BoardListItem> getList(List<BoardEntity> boardEntities) throws Exception {
         List<BoardListItem> boardList = new ArrayList<>();
         
+        // 변경된 것을 반복을 통해서 하나씩 넣음
         for (BoardEntity boardEntity: boardEntities) {
             BoardListItem boardListItem = new BoardListItem(boardEntity);
             boardList.add(boardListItem);
--- back/src/main/java/com/estate/back/repository/BoardRepository.java
+++ back/src/main/java/com/estate/back/repository/BoardRepository.java
@@ -2,9 +2,11 @@ package com.estate.back.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.estate.back.entity.BoardEntity;
 
+@Repository
 // estate 데이터베이스의 board 테이블 작업을 위한 레포지토리
 public interface BoardRepository extends JpaRepository<BoardEntity, Integer>{
     
