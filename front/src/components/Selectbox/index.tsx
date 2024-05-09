f4b8ab095625f0c4e1585bf01b29555c6fff72a1
JiSangChoe <chlwltkd123@gmail.com>
2024-05-09 10:52:17 +0900

    2024.05.09.10:50

---
+++ front/src/components/Selectbox/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css';
+
+//                    component                    //
+export default function SelectBox() {
+    //                    render                    //
+    return (
+    <div>
+        <div className='select-box'>
+            <div className='select-selected'></div>
+            <div className='select-open-button'></div>
+        </div>
+    </div>
+)
+}
